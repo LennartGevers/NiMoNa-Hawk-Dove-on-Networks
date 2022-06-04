5c784e33209afee38f7ae3ef88eba69765f60b0e
lgevers <lgevers@uni-muenster.de>
2022-06-04 23:31:53 +0200

    implemented iterative application of heat equation

--- src/model/heat_equation/heat_equation.py
+++ src/model/heat_equation/heat_equation.py
@@ -1,14 +1,35 @@
-def heat_equation(nodes, a_hawks, a_doves):
-    
+def apply_heat_equation(x_1, x_2, a, dist):
+    return a* (( x_2 - x_1) / dist)
 
-def normalize_nodes(nodes, b = 0):
+def normalize_nodes(nodes):
     #normalizes the vector of hawks and doves of all nodes in respect to the normalization condition
     #that hawks + doves = 1 while maintaining the ratio of hawks and doves in a node.
-    
-    #Optional backgroundfitness for possibility of negative values.
     
     for node in nodes:
 
         value_hawk, value_dove = node.get_value()
         sum_hawk_dove = value_dove + value_hawk
-        node.set_value(((value_hawk+b)/ (sum_hawk_dove + b), (value_dove + b)/(sum_hawk_dove + b)))
+        node.set_value(((value_hawk)/ (sum_hawk_dove), (value_dove)/(sum_hawk_dove)))
+
+
+def heat_equation(nodes, a_hawk, a_dove):
+
+    new_values = []
+
+    for node in nodes:
+        hawk_value, dove_value = node.get_value()
+        hawk_update = 0
+        dove_update = 0
+
+        for connecting_node, distance in node.get_connections():
+            connecting_hawk_value, connecting_dove_value = connecting_node.get_value()
+
+            hawk_update += apply_heat_equation(hawk_value, connecting_hawk_value, a_hawk, distance)
+            dove_update += apply_heat_equation(dove_value, connecting_dove_value, a_dove, distance)
+
+        new_values.append( (hawk_value + hawk_update, dove_value + dove_update))
+
+    for i in range(0,len(nodes)):
+        nodes[i].set_value(new_values[i])
+
+    normalize_nodes(nodes)
