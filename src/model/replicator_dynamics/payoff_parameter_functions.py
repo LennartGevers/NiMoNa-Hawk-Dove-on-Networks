0b9e8e90663282a411d217cfaac5dcc777f311bc
lgevers <lgevers@uni-muenster.de>
2022-06-05 02:56:06 +0200

    added periodic reward

--- src/model/replicator_dynamics/payoff_parameter_functions.py
+++ src/model/replicator_dynamics/payoff_parameter_functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def constant_harm(c):
     def harm(t, value_vector, position):
         return c
@@ -7,5 +9,11 @@ def constant_harm(c):
 def constant_reward(v):
     def reward(t, value_vector, position):
         return v
+
+    return reward
+
+def periodic_reward(a, T):
+    def reward(t, value_vector, position):
+        return a * np.sin(2 * np.pi * (t/T))
 
     return reward
