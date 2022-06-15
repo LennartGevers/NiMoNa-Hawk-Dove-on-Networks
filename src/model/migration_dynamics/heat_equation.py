bf17d38e2e8526af3315a85ac51b30e8f30aa318
lgevers <lgevers@uni-muenster.de>
2022-06-15 20:03:26 +0200

    changed heat equation for use in framework

--- src/model/migration_dynamics/heat_equation.py
+++ src/model/migration_dynamics/heat_equation.py
@@ -3,34 +3,38 @@ from src.model.normalization import norm_environment
 def apply_heat_equation(x_1, x_2, a, dist):
     return a* (( x_2 - x_1) / dist)
 
-def heat_equation(nodes, a_hawk, a_dove, minimum_distance):
 
-    new_values = []
+def heat_equation(a_hawk, a_dove, minimum_distance):
 
-    n = len(nodes)
+    def heat_equation_on_nodes(nodes):
 
-    for node in nodes:
-        hawk_value, dove_value = node.get_value()
-        hawk_update = 0
-        dove_update = 0
+        new_values = []
 
-        n_connections = len(node.get_connections())
+        n = len(nodes)
 
-        if(n_connections):
+        for node in nodes:
+            hawk_value, dove_value = node.get_value()
+            hawk_update = 0
+            dove_update = 0
 
-            for connecting_node, distance in node.get_connections():
-                connecting_hawk_value, connecting_dove_value = connecting_node.get_value()
+            n_connections = len(node.get_connections())
 
-                hawk_update += apply_heat_equation(hawk_value, connecting_hawk_value, a_hawk, distance/minimum_distance)
-                dove_update += apply_heat_equation(dove_value, connecting_dove_value, a_dove, distance/minimum_distance)
+            if(n_connections):
 
-            #new_values.append( (hawk_value*(1 + hawk_update/n_connections), dove_value*(1 + dove_update/n_connections)))
-            new_values.append( (hawk_value + hawk_update, dove_value + dove_update))
+                for connecting_node, distance in node.get_connections():
+                    connecting_hawk_value, connecting_dove_value = connecting_node.get_value()
 
-        else:
-            new_values.append( (hawk_value, dove_value) )
+                    hawk_update += apply_heat_equation(hawk_value, connecting_hawk_value, a_hawk, distance/minimum_distance)
+                    dove_update += apply_heat_equation(dove_value, connecting_dove_value, a_dove, distance/minimum_distance)
 
-    for i in range(0,len(nodes)):
-        nodes[i].set_value(new_values[i])
-    norm_environment(nodes)
+                new_values.append( (hawk_value + hawk_update, dove_value + dove_update))
+
+            else:
+                new_values.append( (hawk_value, dove_value) )
+
+        for i in range(0,len(nodes)):
+            nodes[i].set_value(new_values[i])
+        norm_environment(nodes)
+
+    return heat_equation_on_nodes
     
