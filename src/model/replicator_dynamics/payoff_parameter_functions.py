d5567525155b62314b51b13ca1835bfadbffe41c
lgevers <lgevers@uni-muenster.de>
2022-06-24 19:50:36 +0200

    added periodic harm

--- src/model/replicator_dynamics/payoff_parameter_functions.py
+++ src/model/replicator_dynamics/payoff_parameter_functions.py
@@ -18,6 +18,13 @@ def periodic_reward(a, T, a_0):
 
     return reward
 
+def periodic_harm(a, T, a_0):
+
+    def harm(t, value_vector, position):
+        return a*np.sin((t/T)*2*np.pi) + a_0
+
+    return harm
+
 def periodic_reward_heigth_amplitude(a, T, a_0):
     def reward(t, value_vector, position):
         return a*(position[0]) * np.sin(2 * np.pi * (t/T)) + a_0
