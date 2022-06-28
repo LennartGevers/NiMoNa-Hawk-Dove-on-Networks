c3f6e77defcd44b92af9b7d2ea522937f077cf8e
lgevers <lgevers@uni-muenster.de>
2022-06-28 12:09:02 +0200

    added plot of graph development

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 from src.model.replicator_dynamics.replicator_dynamics import replicator_dynamics
 
@@ -53,10 +54,10 @@ class simulation:
 
             self._save_environment()
 
-        return self.network_history
+        return self.environment
 
 
-    def plot(self, plot_sum=True):
+    def plot_graph(self, plot_sum=True):
         #Erstellt einen Plot einer durchgefuehrten Simulation
         #Plottet die zeitliche Entwicklung der Werte der Netzwerkknoten
 
@@ -68,8 +69,8 @@ class simulation:
 
             #Plot der Entwicklungen der einzelnen Nodes
             for hawk_i, dove_i in self.network_history:
-                plt.plot(t, hawk_i)
-                plt.plot(t, dove_i)
+                plt.plot(t, hawk_i, color='orange')
+                plt.plot(t, dove_i, color='navy')
 
             #Plot der Gesamtentwicklung des Netzwerks (Summen aller Netzwerke)
             if(plot_sum):
@@ -77,9 +78,58 @@ class simulation:
                 sum_hawks = sum( [ np.array(hawk_i) for hawk_i, dove_i in self.network_history])
                 sum_doves = sum( [ np.array(dove_i) for hawk_i, dove_i in self.network_history])
 
-                plt.plot(t, sum_hawks, color='navy', label='Summe Hawks')
-                plt.plot(t, sum_doves, color='orange', label='Summe Doves')
+                plt.plot(t, sum_hawks, color='orange', label='Summe Hawks')
+                plt.plot(t, sum_doves, color='navy', label='Summe Doves')
                 plt.plot(t, np.array(sum_hawks) + np.array(sum_doves), color='black')
                     
         else:
             raise RuntimeError('Die Simulation wurde noch nicht durchgelaufen.')
+
+    def draw_node_animatable(self, ax, node, max_markersize):
+        x, y = node.get_position()
+        val = node.get_value()[0] + node.get_value()[1]
+
+        hawk_share = node.get_value()[0] / (val)
+
+        marker, = ax.plot(x,y, marker='.', markersize=max_markersize*(abs(val)), color=str(hawk_share))
+        return marker
+
+    def draw_connection(self, ax, node_1, node_2):
+        x1, y1 = node_1.get_position()
+        x2, y2 = node_2.get_position()
+
+        ax.plot([x1, x2], [y1, y2], marker='', color='black', linewidth=0.1)
+
+    def plot_network(self, max_markersize=32):
+        t_interval = np.arange(0, len(self.network_history[0][0]))
+        
+        # to run GUI event loop
+        plt.ion()
+
+        figure, ax = plt.subplots()
+
+        markers = []
+
+        #Creating initial Network
+        for node in self.environment:
+
+            for connection in node.get_connections():
+                self.draw_connection(ax, node, connection[0])
+
+            markers.append(self.draw_node_animatable(ax, node, max_markersize*len(self.environment)))
+
+
+        for t in t_interval:
+            for i in range(0, len(self.network_history)):
+
+                val = self.network_history[i][0][t] + self.network_history[i][1][t]
+
+                hawk_share = self.network_history[i][0][t] / (val)
+
+                markers[i].set_markersize(max_markersize*len(self.environment)*(abs(val)))
+                markers[i].set_color(str(hawk_share))
+            
+            figure.canvas.draw()
+            figure.canvas.flush_events()
+            
+            time.sleep(0.1)
